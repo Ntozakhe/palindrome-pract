9388dacad76f07340ec98ec246345d3c20f176ce
Ntozakhe <87692948+Ntozakhe@users.noreply.github.com>
2022-01-16 20:02:48 +0200

    commit again

--- js/site.js
+++ js/site.js
@@ -1,3 +1,27 @@
-function helloWorld(){
-    alert("Hello World");
+function getValue(){
+document.getElementById("alertID").classList.add("invisible"); // ensres that the alert remains invisible when the function starts running
+let userString = document.getElementById("userInput").value;
+userString = userString.replace(/[^a-zA-Z ]/g, "").trim().toLowerCase();
+let revString = reverseString(userString); 
+
+displayString(revString,userString);
 }
+function reverseString(userString){
+    let revString = [];
+    //we'll reverse a string using a for loop.
+    for(let i = userString.length - 1; i >= 0; i--){
+        revString += userString[i];
+        
+    }
+    return revString;  
+    
+    }
+    function displayString(stringRev,stringUser){
+        if(stringRev === stringUser){
+            document.getElementById("message").innerHTML = `Your string reversed is: <strong>${stringRev}</strong>`;
+            document.getElementById("alertID").classList.remove("invisible");
+        }else{
+            document.getElementById("message").innerHTML = `${stringRev} is not a <strong>PALINDROME</strong>`;
+            document.getElementById("alertID").classList.remove("invisible");
+        }
+    }
